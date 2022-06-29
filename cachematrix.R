967c2967282981dd8439352a776c7ad1e6bdc0a2
Lara Sofía Rey <103452728+Larita16@users.noreply.github.com>
2022-06-29 16:38:56 -0300

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,39 @@
 ## functions do
 
 ## Write a short comment describing this function
+##This function allows to create a matrix, set the values of y to x in the
+##parent enviroment and inversa as null. Clear the values of x and inversa. 
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inversa <- NULL
+  set <- function (y) {
+    x <<- y
+    inversa <<- NULL
+  }
+  get <- function () {x}
+  setInversa <- function (inversacalculada) {inversa <<- inversacalculada}
+  getInversa <- function () {inversa}
+  list (set=set, get=get, setInversa=setInversa, getInversa=getInversa)
 }
 
 
 ## Write a short comment describing this function
-
+## This function allows to create the inverse of a matrix and call it inversa, 
+##and to set that value and return it. If it is already been calculated it 
+##returns the previous value. 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inversa <- x$getInversa ()
+  if (!is.null (inversa)) {
+    message ("getting cached data")
+    return (inversa)
+  }
+  data <- x$get ()
+  inversa <- solve (data, ...)
+  x$setInversa (inversa)
+  inversa 
 }
+        ## Return a matrix that is the inverse of 'x'
+x <- makecachematrix (matrix (1:4, nrow=2, ncol=2))
+x
+cacheSolve(x)
+
